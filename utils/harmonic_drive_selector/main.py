86db782c49136992f92c01b66dbc2236906fea65
AimanHaidar <atahalabsi@gmail.com>
2025-08-22 22:49:07 +0300

    connect  funtion to the start button
    the funtion start dialogs for reducer selection

--- utils/harmonic_drive_selector/main.py
+++ utils/harmonic_drive_selector/main.py
@@ -3,6 +3,11 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtWidgets
 from gui.generated.mainwindow import Ui_MainWindow
 from gui.generated.about import Ui_Form
+
+from gui.dialogs.first_selection_dialog import FirstSelectionDialog
+from gui.dialogs.cable_dialog import CableDialog
+from gui.dialogs.selection_input_dialog import SelectionInputDialog
+
 import sys
 from pathlib import Path
 
@@ -19,6 +24,19 @@ class HarmonicSelctorApp(QMainWindow):
         #self.exper_ui.setupUi(self)
         # connect pushButton to a function
         self.ui.about_pushButton.clicked.connect(self.show_about)
+        self.ui.start_pushButton.clicked.connect(self.start_selection)
+
+    def start_selection(self):
+        first_selection_dialog = FirstSelectionDialog()
+        first_selection = first_selection_dialog.exec_()
+        if  first_selection == first_selection_dialog.Accepted:
+            cable_dialog = CableDialog()
+            cable_dialog.exec_()
+        
+        elif first_selection == first_selection_dialog.Rejected:
+            selection_input_dialog = SelectionInputDialog()
+            selection_input_dialog.exec_()
+
 
     def show_about(self):
         # This function is called when the start button is clicked
