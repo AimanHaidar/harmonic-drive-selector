3e833050c1429583f6c96e1cce2f8181a083a75e
AimanHaidar <atahalabsi@gmail.com>
2025-08-22 20:40:47 +0300

    add about implementation  in the mainwindow and launch app from main file

--- utils/harmonic_drive_selector/main.py
+++ utils/harmonic_drive_selector/main.py
@@ -1,7 +1,39 @@
-from data.reducers_tables import df
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtWidgets
+from gui.mainwindow import Ui_MainWindow
+from gui.about import Ui_Form
+import sys
+from pathlib import Path
+
+# Add data to path
+sys.path.append(str(Path(__file__).resolve().parent)+"/src")
+sys.path.append(str(Path(__file__).resolve().parent)+"/gui")
+
+class HarmonicSelctorApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        #self.exper_ui.setupUi(self)
+        # connect pushButton to a function
+        self.ui.about_pushButton.clicked.connect(self.show_about)
+
+    def show_about(self):
+        # This function is called when the start button is clicked
+        self.Form = QtWidgets.QWidget()
+        self.about = Ui_Form()
+        self.about.setupUi(self.Form)
+        self.Form.show()
+
 def main():
     print("Hello from harmonic-drives-selector!")
-    print(df)
+    app = QApplication(sys.argv)
+    window = HarmonicSelctorApp()
+    window.show()
+    sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
