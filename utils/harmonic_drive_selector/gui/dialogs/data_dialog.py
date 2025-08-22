c698e6a3a7f897549ac0f57bdb3da30ca9639162
AimanHaidar <atahalabsi@gmail.com>
2025-08-23 00:47:43 +0300

    make the radio buttons show there crossponding inputs

--- utils/harmonic_drive_selector/gui/dialogs/data_dialog.py
+++ utils/harmonic_drive_selector/gui/dialogs/data_dialog.py
@@ -6,3 +6,15 @@ class DataDialog(QtWidgets.QDialog):
         super().__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.ui.dataTable.hide()
+        self.ui.file_path.hide()
+        self.ui.array_s.clicked.connect(self.show_table)
+        self.ui.json_s.clicked.connect(self.show_file_path)
+    
+    def show_table(self):
+        self.ui.dataTable.show()
+        self.ui.file_path.hide()
+
+    def show_file_path(self):
+        self.ui.file_path.show()
+        self.ui.dataTable.hide()
