1afc8c7b5da2f1c7050ae257e2c4aadc65e9d3cd
AimanHaidar <atahalabsi@gmail.com>
2025-08-23 02:00:20 +0300

    at check funtion to check for invaid input from table

--- utils/harmonic_drive_selector/gui/dialogs/data_dialog.py
+++ utils/harmonic_drive_selector/gui/dialogs/data_dialog.py
@@ -10,6 +10,8 @@ class DataDialog(QtWidgets.QDialog):
         self.ui.file_path.hide()
         self.ui.array_s.clicked.connect(self.show_table)
         self.ui.json_s.clicked.connect(self.show_file_path)
+        self.ui.buttonBox.accepted.disconnect()
+        self.ui.buttonBox.clicked.connect(self.insert_data)
     
     def show_table(self):
         self.ui.dataTable.show()
@@ -18,3 +20,21 @@ class DataDialog(QtWidgets.QDialog):
     def show_file_path(self):
         self.ui.file_path.show()
         self.ui.dataTable.hide()
+
+    def insert_data(self):
+        for row in range(self.ui.dataTable.rowCount()):
+            for col in range(self.ui.dataTable.columnCount()):
+                if row==self.ui.dataTable.rowCount()-1 and col == 0:
+                    continue
+                if row==self.ui.dataTable.rowCount()-1 and col == 1:
+                    continue
+                item = self.ui.dataTable.item(row, col)
+                if item is None or item.text() == "":
+                    print(f"Empty cell at ({row+1},{col+1})")
+                    return
+                try:
+                    float(item.text())
+                except ValueError:
+                    print(f"Invalid float at ({row+1},{col+1}): {item.text()}")
+                    return
+        self.accept()
