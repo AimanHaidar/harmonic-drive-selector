5f4c44d1f0dea0a9942a9fc84e7a1e1ed353b3b1
AimanHaidar <atahalabsi@gmail.com>
2025-08-22 22:47:21 +0300

    connect accept and reject to yes and no buttons

--- utils/harmonic_drive_selector/gui/dialogs/first_selection_dialog.py
+++ utils/harmonic_drive_selector/gui/dialogs/first_selection_dialog.py
@@ -6,3 +6,5 @@ class FirstSelectionDialog(QtWidgets.QDialog):
         super().__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.ui.yes_button.clicked.connect(self.accept)
+        self.ui.no_button.clicked.connect(self.reject)
