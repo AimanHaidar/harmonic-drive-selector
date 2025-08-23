97f187e091b3d827ad3afd6901eefec8e8db8cd2
AimanHaidar <atahalabsi@gmail.com>
2025-08-23 03:39:22 +0300

    add check input for lifetime dialog

--- utils/harmonic_drive_selector/gui/dialogs/input_lifetime_dialog.py
+++ utils/harmonic_drive_selector/gui/dialogs/input_lifetime_dialog.py
@@ -1,10 +1,26 @@
 from PyQt5 import QtWidgets
 from gui.generated.input_lifetime_dialog import Ui_Dialog
+from gui.dialogs.non_numbers_dialog import NonNumbersDialog
 
 class InputLifetimeDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.ui.buttonBox.accepted.connect(self.accept)
+        self.ui.buttonBox.accepted.disconnect()
+        self.ui.buttonBox.accepted.connect(self.check_input)
         self.ui.buttonBox.rejected.connect(self.reject)
+
+    def check_input(self):
+        item = self.ui.lineEdit.text()
+        if item is None or item == "":
+            non_numbers_dialog = NonNumbersDialog()
+            non_numbers_dialog.exec_()
+            return
+        try:
+            self.lifetime = float(item)
+        except ValueError:
+            non_numbers_dialog = NonNumbersDialog()
+            non_numbers_dialog.exec_()
+            return
+        self.accept()
