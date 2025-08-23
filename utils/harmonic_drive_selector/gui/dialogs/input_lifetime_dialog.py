969736ca5998d871e3526d37104b22dce5cf832c
AimanHaidar <atahalabsi@gmail.com>
2025-08-23 03:04:31 +0300

    add lifetime input dialog

---
+++ utils/harmonic_drive_selector/gui/dialogs/input_lifetime_dialog.py
@@ -0,0 +1,10 @@
+from PyQt5 import QtWidgets
+from gui.generated.input_lifetime_dialog import Ui_Dialog
+
+class InputLifetimeDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui.buttonBox.accepted.connect(self.accept)
+        self.ui.buttonBox.rejected.connect(self.reject)
