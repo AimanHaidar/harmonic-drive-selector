de491371e42f23ad730cc3a6f58809e28d90c164
AimanHaidar <atahalabsi@gmail.com>
2025-08-23 03:39:42 +0300

    set initialization for the data dialog

--- utils/harmonic_drive_selector/gui/dialogs/data_dialog.py
+++ utils/harmonic_drive_selector/gui/dialogs/data_dialog.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets
 from gui.generated.data_dialog import Ui_Dialog
 from gui.dialogs.non_numbers_dialog import NonNumbersDialog
+from PyQt5.QtWidgets import QTableWidgetItem
 
 class DataDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
@@ -9,22 +10,44 @@ class DataDialog(QtWidgets.QDialog):
         self.ui.setupUi(self)
         self.ui.dataTable.hide()
         self.ui.file_path.hide()
+        self.ui.label_2.hide()
         self.ui.array_s.clicked.connect(self.show_table)
         self.ui.json_s.clicked.connect(self.show_file_path)
         self.ui.buttonBox.accepted.disconnect()
-        self.ui.buttonBox.clicked.connect(self.insert_data)
-    
+        self.ui.buttonBox.accepted.connect(self.insert_data)
+        self.float_data = [[0]*self.ui.dataTable.columnCount()]*self.ui.dataTable.rowCount()
+
+        self.ui.dataTable.setItem(0, 0, QTableWidgetItem("400"))
+        self.ui.dataTable.setItem(1, 0, QTableWidgetItem("320"))
+        self.ui.dataTable.setItem(2, 0, QTableWidgetItem("200"))
+        self.ui.dataTable.setItem(3, 0, QTableWidgetItem("1000"))
+
+        # Second column
+        self.ui.dataTable.setItem(0, 2, QTableWidgetItem("0.3"))
+        self.ui.dataTable.setItem(1, 2, QTableWidgetItem("3.0"))
+        self.ui.dataTable.setItem(2, 2, QTableWidgetItem("0.4"))
+        self.ui.dataTable.setItem(3, 2, QTableWidgetItem("0.15"))
+        self.ui.dataTable.setItem(4, 2, QTableWidgetItem("0.2"))
+
+        # Third column
+        self.ui.dataTable.setItem(0, 1, QTableWidgetItem("7"))
+        self.ui.dataTable.setItem(1, 1, QTableWidgetItem("14"))
+        self.ui.dataTable.setItem(2, 1, QTableWidgetItem("7"))
+        self.ui.dataTable.setItem(3, 1, QTableWidgetItem("14"))
+
     def show_table(self):
         self.ui.dataTable.show()
         self.ui.file_path.hide()
+        self.ui.label_2.hide()
 
     def show_file_path(self):
         self.ui.file_path.show()
         self.ui.dataTable.hide()
+        self.ui.label_2.show()
 
     def insert_data(self):
-        for row in range(self.ui.dataTable.rowCount()):
-            for col in range(self.ui.dataTable.columnCount()):
+        for col in range(self.ui.dataTable.columnCount()):
+            for row in range(self.ui.dataTable.rowCount()):
                 if row==self.ui.dataTable.rowCount()-1 and col == 0:
                     continue
                 if row==self.ui.dataTable.rowCount()-1 and col == 1:
@@ -36,7 +59,7 @@ class DataDialog(QtWidgets.QDialog):
                     non_numbers_dialog.exec_()
                     return
                 try:
-                    float(item.text())
+                    self.float_data[row][col] = float(item.text())
                 except ValueError:
                     print(f"Invalid float at ({row+1},{col+1}): {item.text()}")
                     non_numbers_dialog = NonNumbersDialog()
