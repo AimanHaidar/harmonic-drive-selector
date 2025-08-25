e9276a1b88a3ace3fdacf198bacfdaafb0ceb2f3
AimanHaidar <atahalabsi@gmail.com>
2025-08-25 03:13:30 +0300

    seprate closing from press no

--- gui/dialogs/cable_dialog.py
+++ gui/dialogs/cable_dialog.py
@@ -6,5 +6,13 @@ class CableDialog(QtWidgets.QDialog):
         super().__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.ui.yes_button.clicked.connect(self.accept)
-        self.ui.no_button.clicked.connect(self.reject)
+        self.ui.yes_button.clicked.connect(self.yes)
+        self.ui.no_button.clicked.connect(self.no)
+
+    def yes(self):
+        self.answer = True
+        self.accept()
+        
+    def no(self):
+        self.answer = False
+        self.accept() 
