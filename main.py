737779944d6d567d4f973ae95ddb31b1d1ecc307
AimanHaidar <atahalabsi@gmail.com>
2025-08-25 03:12:52 +0300

    Refactor code structure for improved readability and maintainability and add stiffness base dimensioning

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ from gui.dialogs.data_dialog import DataDialog
 from gui.dialogs.input_lifetime_dialog import InputLifetimeDialog
 from gui.dialogs.non_numbers_dialog import NonNumbersDialog
 from gui.dialogs.result_dialog import ResultDialog
+from gui.dialogs.drive_application_dialog import DriveApplicationDialog
 
-from src.harmonic_dasigner import torque_based_dimensioning
+from src.harmonic_dasigner import torque_based_dimensioning,stiffness_based_dimensioning,output_bearing_dimensioning
 from data.reducers_tables import reducers_df
 
 import sys
@@ -44,34 +45,49 @@ class HarmonicSelctorApp(QMainWindow):
         # create dialog it ask user if he had selected a harmonic drive before or not
         first_selection_dialog = FirstSelectionDialog()
         first_selection = first_selection_dialog.exec_()
-
-        proceed = True
+        if first_selection == first_selection_dialog.Rejected:
+            return
+        #this boolean to know if the user selected a harmonic drive
+        #to determine how to input to torque_based_dimensioning function
         first_selection_mode = False
-        if  first_selection == first_selection_dialog.Rejected:
+        #to know weather  the user push yes or no in first selection dialog
+        if  first_selection_dialog.answer == False:
+            #ask user if he want to select SHG or CSG
+            #if yes SHG if no CSG
             cable_dialog = CableDialog()
             with_cable = cable_dialog.exec_()
-            if with_cable == cable_dialog.Accepted:
+            # if the user closed the dialog or pushed cancel close the selection process
+            if with_cable == cable_dialog.Rejected:
+                return
+            if cable_dialog.answer == True:
                 self.type = "SHG"
+                #use the infrom_type that generate dialog to inform the user with his selection
                 self.infrom_type("you choosed SHG.do you want to proceed selecting with this type?")
             else:
                 self.type = "CSG"
                 self.infrom_type("you choosed CSG.do you want to proceed selecting with this type?")
 
+            #from infrom_type function we get if the user accepted or rejected
             if not self.informed:
                 return
         
-        elif first_selection == first_selection_dialog.Accepted:
+        # if the user selected yes in first selection dialog
+        elif first_selection_dialog.answer == True:
+            #dialog for user to  input the first selection
             selection_input_dialog = SelectionInputDialog()
             state = selection_input_dialog.exec_()
             try:
+                #get the first selection from the dialog
                 self.first_selection = selection_input_dialog.first_selection
                 first_selection_mode = True
             except AttributeError:
                 pass
+            # if the user closed the dialog or pushed cancel close the selection process
             if selection_input_dialog.Rejected == state:
                 return
-
+        # get the data from the data dialog
         self.data = self.input_data()
+        #close selection process if no data is input is cancelled
         if not self.data:
             return
         # Combine T and n into one load_data dictionary
@@ -85,45 +101,78 @@ class HarmonicSelctorApp(QMainWindow):
             'n_k': self.data[3][1]
         }
 
-
+        #dialog to input the required lifetime of the harmonic drive
         input_lifetime_dialog = InputLifetimeDialog()
         state = input_lifetime_dialog.exec_()
+        #close if user cancelled
         if state == input_lifetime_dialog.Rejected:
             return
         self.lifetime = input_lifetime_dialog.lifetime
         if first_selection_mode:
-            self.selection = torque_based_dimensioning(
+            #do the selection based on the first selection
+            #run selection algorithm
+            self.selection1 = torque_based_dimensioning(
                 self.first_selection['Series'],
                 self.load_data,
                 self.lifetime,
                 self.first_selection
             )
+
         else:
-            self.selection = torque_based_dimensioning(
+            #do the selection based on the type only
+            self.selection1 = torque_based_dimensioning(
                 self.type,
                 self.load_data,
                 self.lifetime
             )
         
-        print(self.selection)
-        drive_specs = reducers_df[(reducers_df["Series"] == self.selection.split("-")[0]) & (reducers_df["Size"] == int(self.selection.split("-")[1])) & (reducers_df["Ratio"] == int(self.selection.split("-")[2]))]
-        drive_specs = drive_specs.to_numpy().transpose()
-        result_dialog = ResultDialog()
-        result_dialog.fill_table(result_dialog.data_model,result_dialog.ui.dataTableView,self.data)
-        result_dialog.fill_table(result_dialog.specs_model,result_dialog.ui.driveTableView,drive_specs)
-        result_dialog.ui.selection.setText((self.selection))
-        result_dialog.ui.drive_photo.setPixmap(QtGui.QPixmap(":/pictures/pictures/"+self.selection.split("-")[0]+"_photo.png"))
-        result_dialog.exec_()
+        self.show_result(self.selection1)
+
+        if self.continue_dimensioning:
+            #open the drive application dialog to get the type of application
+            drive_application_dialog = DriveApplicationDialog()
+            state = drive_application_dialog.exec_()
+            if state == drive_application_dialog.Rejected:
+                return
+            application_type = drive_application_dialog.ui.application_type.currentText()
+            load_moment_of_inertia_dialog = InputLifetimeDialog()
+            load_moment_of_inertia_dialog.ui.label.setText("your load moment of inertia")
+            state = load_moment_of_inertia_dialog.exec_()
+            if state == load_moment_of_inertia_dialog.Rejected:
+                return
+            load_moment_of_inertia = load_moment_of_inertia_dialog.lifetime
+
+            self.selection2 = stiffness_based_dimensioning(self.selection1,application_type,load_moment_of_inertia)
+            self.selection2 = torque_based_dimensioning(self.selection2.split("-")[0],
+                                                        self.load_data,
+                                                        self.lifetime,
+                                                        {
+                                                         "Series":self.selection2.split("-")[0],
+                                                         "Size":int(self.selection2.split("-")[1]),
+                                                         "Ratio":int(self.selection2.split("-")[2])
+                                                         }
+                                                         )
+            self.show_result(self.selection2)
+
+            
+            
+
+
             
 
     def show_about(self):
-        # This function is called when the start button is clicked
+        '''This function is called when the about button in mainwindow is clicked '''
         self.Form = QtWidgets.QWidget()
         self.about = Ui_Form()
         self.about.setupUi(self.Form)
         self.Form.show()
     
     def input_data(self):
+        '''This function opens the data input dialog and returns the data as a list of lists
+        
+        Returns:
+            list of data or False if dialog is cancelled
+        '''
         data_dialog = DataDialog()
         state = data_dialog.exec_()
         if state == data_dialog.Accepted:
@@ -135,6 +184,20 @@ class HarmonicSelctorApp(QMainWindow):
         type_inform_dialog = TypeInformDialog()
         type_inform_dialog.ui.harmonic_type.setText(type)
         self.informed = type_inform_dialog.exec_()
+
+    def show_result(self,selection):
+        print(selection)
+        drive_specs = reducers_df[(reducers_df["Series"] == selection.split("-")[0]) & (reducers_df["Size"] == int(selection.split("-")[1])) & (reducers_df["Ratio"] == int(selection.split("-")[2]))]
+        drive_specs = drive_specs.to_numpy().transpose()
+        result_dialog = ResultDialog()
+        result_dialog.fill_table(result_dialog.data_model,result_dialog.ui.dataTableView,self.data)
+        result_dialog.fill_table(result_dialog.specs_model,result_dialog.ui.driveTableView,drive_specs)
+        result_dialog.ui.selection.setText((selection))
+        if selection.split("-")[0] in ["HFUC","CSG"]:
+            result_dialog.ui.drive_photo.setPixmap(QtGui.QPixmap(":/pictures/pictures/CSG_photo.png"))
+        else:
+            result_dialog.ui.drive_photo.setPixmap(QtGui.QPixmap(":/pictures/pictures/SHG_photo.png"))
+        self.continue_dimensioning = (result_dialog.exec_() == result_dialog.Accepted)
 
 def main():
     print("Hello from harmonic-drives-selector!")
