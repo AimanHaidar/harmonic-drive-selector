158a9c6b6d2b695fe57f9ffab0cd259ec0f066ed
AimanHaidar <atahalabsi@gmail.com>
2025-08-27 01:12:45 +0300

    add info dialogs

---
+++ gui/dialogs/torque_info_dialog.py
@@ -0,0 +1,8 @@
+from PyQt5 import QtWidgets
+from gui.generated.torque_info_dialog import Ui_Dialog
+
+class TorqueInfoDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
