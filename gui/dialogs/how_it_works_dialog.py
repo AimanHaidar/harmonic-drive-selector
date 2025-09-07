661440e3ecb0e727fc0e536bc39c098064326d61
Aiman Haidar <atahalabsi@gmail.com>
2025-09-07 20:46:57 +0300

    make how it works dialog maximazable

--- gui/dialogs/how_it_works_dialog.py
+++ gui/dialogs/how_it_works_dialog.py
@@ -1,8 +1,16 @@
 from PyQt5 import QtWidgets
 from gui.generated.how_it_works_dialog import Ui_Dialog
+from PyQt5.QtCore import Qt
 
 class HowItWorksDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.setWindowFlags(
+            Qt.Window |
+            Qt.WindowMinimizeButtonHint|
+            Qt.WindowMaximizeButtonHint|
+            Qt.WindowCloseButtonHint
+            )
+        self.showMaximized()
